4688a2f00a1da005da2fbcd02cf14bdb48955c1e
katchysyd <kachi_side@yahoo.com>
2024-03-18 18:48:48 -0500

    file

--- data.tf
+++
@@ -1,6 +0,0 @@
-data "aws_security_group" "demo-sec" {
- # id = var.aws_security_group
- // data code to pull or call infrascsture already existing in aws
-  name = "webserver"
-  id = "sg-01aee5d9921590640"
-}
